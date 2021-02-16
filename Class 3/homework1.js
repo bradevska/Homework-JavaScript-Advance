2091f7369a650ed62562de2dd55c3c6625e6c4cd
biljana.radevska <b.radevska@yahoo.com>
2021-02-16 23:01:08 +0100

    Change16.02.2021

--- Class 3/homework1.js
+++ Class 3/homework1.js
@@ -6,61 +6,53 @@
 //**URL:** https://raw.githubusercontent.com/Drakso/AJS2019/master/Class1/students.json \
 //**NOTE:** You need to parse this data before using it.
 
-//## Exercise 2
-//Create a button
-//When the button is clicked, call the StarWars api for the first person. \
-//Print the person name in an **h1** tag. \
-//Print the person stats in a **table**:
-//* Height
-//* Weight
-//* Eye color
-//* Hair color
-
-//**URL:** https://swapi.dev/api/people/1 \
-//**NOTE:** JQuery will autmatically parse this call (js will not).
 
  //Exercise 1
 
  $(document).ready(function() {
     document.getElementById("request").addEventListener("click",function() {
             $.ajax({
-                type:"GET" ,
+                method:"GET" ,
                 url:  "https://raw.githubusercontent.com/Drakso/AJS2019/master/Class1/students.json",  
                 success: function(response) {
                     console.log("Request is successfull");
                     let responseObject=JSON.parse(response);
                     console.log(responseObject);
                 },
-                error: function(response) {
+                error: function (response) {
                     console.log("Bad Request");
-                }
+            }
 
-            })
+        
+        })
+
+            
     });
+    
 
-    function printNameOfAcademy (element,response) {
-        let elementDIV= $(element);//("#nameOfAcademy")
-        elementDIV.html("");
-        //for(let i=0; i<response.academy.length; i++) {
-            elementDIV.append(`<h1> ${response.academy} </h1>`);
-        //}
+    function printNameOfAcademy (response) {
+        let element= $("#nameOfAcademy");//("#nameOfAcademy")
+        element.html("");
+        
+         element.append(`<h1> ${response.academy.name} </h1>`);
+        
     }
-    printNameOfAcademy("#nameOfAcademy",response);
+     printNameOfAcademy(response);
 
-    function printNamesOfStudents (element,response) {
-        let elementUL=$(element);//("#nameOfStudents")
-        elementUL.html("");
-        for(let i =0; i<response.studensts.length; i++) {
-            elementUL.append( `<ul>  ${response.studensts.length[i]} </ul>`)
+
+     function printNameOfStudents (element,respons) {
+         let list=$("nameOfStudents");
+         list.innerHTML="";
+         for( let i=0;i<students.length; i++) {
+             list.append(`<li> ${response.students[i].name}</li>`);
+         }
+     
         }
-
-    }
-    printNamesOfStudents("#nameOfStudents",response);
-
-
+    
+    printNameOfStudents("#nameOfStudents",response);
+    
 
 
 
 
-
-})
+    });
---
+++ Class 3/homework2.js
@@ -0,0 +1,12 @@
+//## Exercise 2
+//Create a button
+//When the button is clicked, call the StarWars api for the first person. \
+//Print the person name in an **h1** tag. \
+//Print the person stats in a **table**:
+//* Height
+//* Weight
+//* Eye color
+//* Hair color
+
+//**URL:** https://swapi.dev/api/people/1 \
+//**NOTE:** JQuery will autmatically parse this call (js will not).
