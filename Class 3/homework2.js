53809f2f2ed59d311b1b65e1b2b3057aa0fa0c63
biljana.radevska <b.radevska@yahoo.com>
2021-02-17 23:19:58 +0100

    17.02.2021

--- Class 3/homework2.js
+++ Class 3/homework2.js
@@ -10,3 +10,74 @@
 
 //**URL:** https://swapi.dev/api/people/1 \
 //**NOTE:** JQuery will autmatically parse this call (js will not).
+
+//Exercise 2
+
+
+$(document).ready(function(){
+    document.getElementById("request").addEventListener("click",function(){
+        $.ajax({
+            method:"GET",
+            url: "https://swapi.dev/api/people/1",
+            success: function(response) {
+                console.log(response);
+                let element1=document.getElementById("personName");
+                element1.innerHTML="";
+                element1.innerHTML += (`${response.name}`);
+
+                let element2= document.getElementById("personStats");
+                element2.innerHTML="";
+                element2.innerHTML += (`<table border="2px">
+
+                <tr>
+                <td>Height</td>
+                <td>Mass</td>
+                <td>Hair color</td> 
+                <td>Eye color</td>
+                </tr>
+
+                <tr>
+                <td>${response.height}</td>
+                <td>${response.mass}</td>
+                <td>${response.hair_color}</td> 
+                <td>${response.eye_color}</td>
+                </tr>;
+                </table>`);
+
+            },
+            error: function(response){
+                console.log("Bad request");
+            }
+        })
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+})
+
+
+//parsing elements to HTML with VanillaJS(Pure Javascript)
+                // let element= document.getElementById("astros");
+                // element.innerHTML="";
+
+                // for(let i=0;i<response.people.length;i++){
+                //     element.innerHTML+=`<li>${response.people[i].name}</li>`;
+                // }
+
+                //parsing elements to HTML with jQuery
+                // let elementWithJquery=$("#astros");
+                // elementWithJquery.html("");
+                // for(let i=0;i<response.people.length;i++){
+                //     elementWithJquery.append(`<li>${response.people[i].name}</li>`);
+                // }
